d5ad482102be3bff8dfe9b23bb4391fb40d6b33d
harshitguptaaa <harshgupta8756@gmail.com>
2025-06-19 12:14:19 +0530

    Initial commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,75 @@
+<template>
+  <img src="@/assets/logo.avif" alt="Logo" class="logo" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input v-model="username" type="text" placeholder="Username" />
+    <input v-model="email" type="email" placeholder="Email" />
+    <input v-model="password" type="password" placeholder="Password" />
+    <button type="submit" v-on:click="signUp">Sign Up</button>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async signUp() {
+      const result = await axios.post('http://localhost:3000/users', {
+        name: this.username,
+        email: this.email,
+        password: this.password,
+      })
+      console.warn(result)
+      if (result.status === 201) {
+        alert('User created successfully!')
+        localStorage.setItem('user-info', JSON.stringify(result.data))
+      } else {
+        alert('Error creating user')
+      }
+    },
+  },
+}
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  color: whitesmoke;
+  font-size: 2em;
+}
+.logo {
+  width: 100px;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 20px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 2px solid red;
+}
+.register button {
+  width: 300px;
+  height: 40px;
+  display: block;
+  margin-bottom: 20px;
+  margin-right: auto;
+  margin-left: auto;
+  background-color: red;
+  color: white;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to the Restaurant Management System</h1>
+    <p>Please sign up or log in to continue.</p>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'Home-vue',
+  data() {
+    return {
+      message: 'Welcome to the Restaurant Management System',
+    }
+  },
+  mounted() {
+    console.log(this.message)
+  },
+}
+</script>
